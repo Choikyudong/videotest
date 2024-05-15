29eaefb064028b074c08ce425d18036030057d9b
Choikyudong <slgmgm@naver.com>
2024-05-15 23:23:23 +0900

    Initial commit

---
+++ src/main/java/com/videotest/StreamingService.java
@@ -0,0 +1,22 @@
+package com.videotest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class StreamingService {
+
+	private static final String FORMAT = "classpath:videos/%s.mp4";
+
+	@Autowired
+	private ResourceLoader resourceLoader;
+
+	// 비디오 하나를 가져옴
+	public Mono<Resource> getVideo(String title) {
+		return Mono.fromSupplier(() -> resourceLoader.getResource(String.format(FORMAT, title)));
+	}
+
+}
