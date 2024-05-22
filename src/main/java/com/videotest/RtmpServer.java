6d75a712d303716b9543b6bc6d4e54dfc5e51c04
Choi.kyudong <slgmgm@naonsoft.com>
2024-05-22 14:00:19 +0900

    rtmp 서버 테스트 - 1

---
+++ src/main/java/com/videotest/RtmpServer.java
@@ -0,0 +1,40 @@
+package com.videotest;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+// 송신서버
+public class RtmpServer {
+
+	public void start() throws InterruptedException {
+		EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(bossGroup, workerGroup)
+					.channel(NioServerSocketChannel.class)
+					.childHandler(new ChannelInitializer<SocketChannel>() {
+						@Override
+						public void initChannel(SocketChannel ch) throws Exception {
+							ch.pipeline().addLast(new RtmpServerHandler());
+						}
+					})
+					.option(ChannelOption.SO_BACKLOG, 128)
+					.childOption(ChannelOption.SO_KEEPALIVE, true);
+
+			int port = 1935;
+			ChannelFuture f = b.bind(port).sync();
+			f.channel().closeFuture().sync();
+		} finally {
+			workerGroup.shutdownGracefully();
+			bossGroup.shutdownGracefully();
+		}
+	}
+
+}
--- src/main/java/com/videotest/VideotestApplication.java
+++ src/main/java/com/videotest/VideotestApplication.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
@@ -23,8 +26,32 @@ public class VideotestApplication {
 		return service.getVideo(title);
 	}
 
-	public static void main(String[] args) {
+	@GetMapping(value = "hello")
+	public Mono<String> getViedo() {
+		return Mono.just("안녕");
+	}
+
+	// 전통적인 방법
+	@GetMapping("/sync")
+	public ResponseEntity<String> syncRequest() {
+		RestTemplate restTemplate = new RestTemplate();
+		String response = restTemplate.getForObject("https://naver.com", String.class);
+		return ResponseEntity.ok(response);
+	}
+
+	// 리액티브 방법
+	@GetMapping("/reactive")
+	public Mono<ResponseEntity<String>> reactiveRequest() {
+		WebClient webClient = WebClient.create();
+		return webClient.get()
+				.uri("https://naver.com")
+				.retrieve()
+				.toEntity(String.class);
+	}
+
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(VideotestApplication.class, args);
+		new RtmpServer().start();
 	}
 
 }
